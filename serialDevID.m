ac0ca139295d286f86a7f95a46f0b6688c612197
Enrico Segre <enrico.segre@weizmann.ac.il>
2020-07-06 12:28:02 +0300

    serialDevID for multiple ports at once

--- serialDevID.m
+++ serialDevID.m
@@ -1,21 +1,36 @@
-function IDstring=serialDevID(port)
-
+function [IDstrings,ports]=serialDevID(ports)
 % find identifier names for USB-serial converters connected to the USB
 %  ports. A problem with non-serial locked drivers is that the enumeration
-% of a dongle connected to an arbitrarily port is can take any value.
+% of a dongle connected to an arbitrarily port can take any value.
 
-% port: character array, e.g. '/dev/ttyUSB0' or '/dev/ttyACM0'
+% port: single character or string array, e.g. '/dev/ttyUSB0', for a single port
+%       or string array, e.g. ["/dev/ttyUSB0","/dev/ttyACM0"], serialportlist('all')
 
-% in Linux (ubuntu XX):
+% Empty IDstrings are returned for nonexisting ports or ports which are not
+%   virtual ports created by USB converters, e.g. real physical ports (/dev/ttyS*) 
 
-% strip '/dev/'
-sport=strrep(port,'/dev/','');
+% Example: [IDs,names]=serialDevID(serialportlist)
 
-IDstring='';
+% using strings, i.e. working only in matlab>2017a
 
-if exist('/dev/serial/by-id','file')
-    [~,IDstring]=unix(['find /dev/serial/by-id -type l -printf ''%l %p\n'' |grep ' ...
-                          sport '| cut -d" " -f2']);
+% In Linux (ubuntu XX): resolve the symlinks in the pseudodir /dev/serial/by-id 
+
+% set up for single or multiple ports
+if isa(ports,'string') && length(ports)>1
+    nports=length(ports);
+else
+    nports=1;
+    ports=string(ports);
 end
+IDstrings(1:nports)="";
 
-IDstring=strrep(IDstring(1:end-1),'/dev/serial/by-id/','');
+for i=1:nports
+    % strip '/dev/'
+    portID=strrep(char(ports(i)),'/dev/','');
+    IDstring='';
+    if exist('/dev/serial/by-id','file')
+        [~,IDstring]=unix(['find /dev/serial/by-id -type l -printf ''%l %p\n'' |grep ' ...
+            portID '| cut -d" " -f2']);
+    end    
+    IDstrings(i)=strrep(IDstring(1:end-1),'/dev/serial/by-id/','');
+end
