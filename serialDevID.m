17ff0a47d27cf29bdbd73cdaa37fb4c4b58e3675
Enrico Segre <enrico.segre@weizmann.ac.il>
2020-07-06 13:52:25 +0300

    formatted help preamble

--- serialDevID.m
+++ serialDevID.m
@@ -1,17 +1,17 @@
 function [IDstrings,ports]=serialDevID(ports)
-% find identifier names for USB-serial converters connected to the USB
+% Find identifier names for USB-serial converters connected to the USB
 %  ports. A problem with non-serial locked drivers is that the enumeration
-% of a dongle connected to an arbitrarily port can take any value.
-
+%  of a dongle connected to an arbitrarily port can take any value.
+%
 % port: single character or string array, e.g. '/dev/ttyUSB0', for a single port
 %       or string array, e.g. ["/dev/ttyUSB0","/dev/ttyACM0"], serialportlist('all')
-
+%
 % Empty IDstrings are returned for nonexisting ports or ports which are not
 %   virtual ports created by USB converters, e.g. real physical ports (/dev/ttyS*) 
-
+%
 % Example: [IDs,names]=serialDevID(serialportlist)
-
-% using strings, i.e. working only in matlab>2017a
+%
+% Using strings, i.e. working only in matlab>2017a
 
 % In Linux (ubuntu XX): resolve the symlinks in the pseudodir /dev/serial/by-id 
 
